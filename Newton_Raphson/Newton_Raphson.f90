ee498a69e381a21831e53ac21f0d1bb5d679865a
John Woolliams <john.woolliams@roslin.ed.ac.uk>
2017-10-15 22:37:57 +0100

    Simple square root

--- Newton_Raphson/Newton_Raphson.f90
+++ Newton_Raphson/Newton_Raphson.f90
@@ -1,19 +1,26 @@
 program newton_raphson
 implicit none
 integer, parameter :: dp=kind(0.0d0)
-real(kind=dp) :: x,fx,fdash,tol,xdel,c,xl,fl,xu,fu,dx
+real(kind=dp) :: x,fx,fdash,tol,xl,fl,xu,fu,dx,dxold,c
 character(len=1) :: aflag
 integer :: i
 !
 tol=1.d-6
+print *, " ... tolerance is set to ",tol
+print *, " ... do you wish to change tolerance? (y/n) ... "
+read(*,*) aflag
+if(aflag=="y".or.aflag=="Y") then
+    print *, " ... enter value ... "
+    read(*,*) tol
+end if
 values: do
     i=0
-    xdel=huge(x)
+    dx=huge(x)
     print '(a)', " ... enter target value ... "
     read(*,*) c
     print '(a)', " ... enter initial estimate of root ... "
     read(*,*) x
-    solve: do while (xdel>tol)
+    solve: do while (dx>tol)
         i=i+1
         xl=x-2.d0*tol
         fl=func(xl,c)
@@ -23,14 +30,15 @@ values: do
         fx=func(x,c)
         dx=fx/fdash
         x=x-dx
-        if(i>3.and.abs(dx)>xdel) then ! not converging
+        dx=abs(dx)
+        if(i>3.and.dx>dxold) then ! not converging
             print '(a)', " ... algorithm is diverging ... "
             print '(/a)', " ... do you wish another target value? (y/n) ... "
             read(*,*) aflag
             if(aflag=="n".or.aflag=="N") exit values
             cycle values
         end if
-        xdel=abs(dx)
+        dxold=dx
         print '(f12.6)', x
     end do solve
     print '(a,f12.6)', " ... convergence criterion reached with root ... ",x
